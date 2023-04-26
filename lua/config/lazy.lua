fb619f9db8a7990d1c35e51b3a40b93277d99bf3
ZCDu <ubuntu@wsl.com>
2023-04-26 17:50:02 +0800

    change lazyvim from gitee to github

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -2,7 +2,7 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://gitee.com/zcdu/lazy-vim.git", "--branch=main", lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/ZCDu/Lazyvim.git", "--branch=main", lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
