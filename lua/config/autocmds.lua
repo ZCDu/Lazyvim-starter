226f69f666b0dba1d6f04cf88994a5d90c5dda4b
zcdu <zcdu@foxmail.com>
2025-01-01 14:04:47 +0800

    refactor(app): 增加对jumplist的优化配置

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,9 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- 针对neovim的jumplist的配置，就是gd ctrl-o行为的优化，可以通过jumps查看jumplsit
+vim.cmd([[
+set tagfunc=v:lua.vim.lsp.tagfunc
+set jumpoptions+=stack
+]])
