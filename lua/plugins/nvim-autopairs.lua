58085d26f2b5100977a1e3dbba112ba1f6c6b0a0
zcdu <zcdu@foxmail.com>
2025-01-23 21:16:39 +0800

    fix(tools): nvim-autopairs nvim-autopairs 直接用基础的配置就完事了

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -2,36 +2,7 @@
 local M = {
   "windwp/nvim-autopairs",
   event = "VeryLazy",
+  config = true,
 }
-
-M.config = function ()
-  require("nvim-autopairs").setup {
-    check_ts = true,
-    ts_config = {
-      lua = { "string", "source" },
-      javascript = { "string", "template_string" },
-      java = false,
-    },
-    disable_filetype = { "TelescopePrompt", "spectre_panel" },
-    fast_wrap = {
-      map = "<M-e>",
-      chars = { "{", "[", "(", '"', "'" },
-      pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-      offset = 0, -- Offset from pattern match
-      end_key = "$",
-      keys = "qwertyuiopzxcvbnmasdfghjkl",
-      check_comma = true,
-      highlight = "PmenuSel",
-      highlight_grey = "LineNr",
-    },
-  }
-
-  local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-  local cmp_status_ok, cmp = pcall(require, "nvim-cmp")
-  if not cmp_status_ok then
-    return
-  end
-  cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
-end
 
 return M
